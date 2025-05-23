83b192da82690372230515995f6424dd515d878c
dudulimavasc84 <lima65913@gmail.com>
2025-05-23 17:28:23 -0400

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 
 const { createClient } = supabase;
-const supabaseUrl = 'https://SEU_PROJETO.supabase.co';
-const supabaseKey = 'SEU_PUBLIC_ANON_KEY';
+const supabaseUrl = 'https://pndtmbbajirapzwkkxcv.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBuZHRtYmJhamlyYXB6d2treGN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgwMjIzNTEsImV4cCI6MjA2MzU5ODM1MX0.ZivS91X8PZAMaWc5ySzCVM1MNL5zl6yLwamVqgQe7ik';
 const supabase = createClient(supabaseUrl, supabaseKey);
