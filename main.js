194f694af323f1ca93dd76bceb66b7713aff6b28
dudulimavasc84 <lima65913@gmail.com>
2025-05-23 13:40:55 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 
+function login() {
+    const user = document.getElementById('user').value;
+    const pass = document.getElementById('pass').value;
+    if (user === 'Dudu' && pass === 'fer111984') {
+        document.getElementById('login-container').style.display = 'none';
+        document.getElementById('app-container').style.display = 'block';
+    } else {
+        document.getElementById('login-status').innerText = 'Usuário ou senha inválidos.';
+    }
+}
+
+function logout() {
+    document.getElementById('app-container').style.display = 'none';
+    document.getElementById('login-container').style.display = 'block';
+    document.getElementById('login-status').innerText = '';
+}
+
 function showPage(pageId) {
     ['painel', 'lancamentos', 'estoque', 'historico', 'relatorios'].forEach(id => {
         document.getElementById(id).style.display = (id === pageId) ? 'block' : 'none';
